5d6eb8064f32e4d47e385e377f450f5989a420a3
Aditya Agarwal <adityaagarwal.cse20@jecrc.ac.in>
2019-02-09 12:15:48 +0530

    add reducers for the app

---
+++ src/reducers/todos.js
@@ -0,0 +1,27 @@
+import {
+  Add_TODO,
+  REMOVE_TODO,
+  TOGGLE_TODO
+} from '../actions/todos'
+
+export default function todos(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return state.concat([action.todo])
+
+    case TOGGLE_TODO:
+      return state.map((todo) => todo.id !== action.id ? todo :
+        Object.assign({}, todo, {
+          complete: !todo.complete
+        }))
+
+    case REMOVE_TODO:
+      return state.filter((todo) => todo.id !== action.id)
+
+    case RECEIVE_DATA:
+      return action.todos
+
+    default:
+      return state
+  }
+}
